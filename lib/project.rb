633d1a22ac0ae4519c4ced315f6a18c80b40f8dd
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 22:19:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,10 +7,10 @@ class Project
     self.backers = []
   end
 
-# def add_backer(backer)
-#   self.backers << backer
-#   backer.backed_projects << self
-# end
+def add_backer(backer)
+  self.backers << backer
+  backer.backed_projects << self
+end
 
 
 
